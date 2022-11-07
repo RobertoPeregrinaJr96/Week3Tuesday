44c2f2e5a9f8379b237792413a9938485cec0374
Roberto Peregrina <111211515+RobertoPeregrinaJr96@users.noreply.github.com>
2022-11-06 17:49:16 -0800

    some addition

--- practice-for-week-03-recursion-pt1-practice/problems/01-sum-to-n.js
+++ practice-for-week-03-recursion-pt1-practice/problems/01-sum-to-n.js
@@ -9,42 +9,29 @@ number below 0.
 declare a fucntion sumToN with the parameter of n
 the function should take the argument of n and return the sum of all the iterations from 0
 if there is a falsy value present then return null
-
-edge case : if n < 0
-base case : if n === 0
-recursive step : n - 1 : total + 1
-resursive base : sumToN(n - 1)
 */
 // -----------Using a for loop to see how it should iterate ----------
-// if (n < 0) {
-//   return null
-// }
-// for (let i = 0; i <= n; i++) {
-//   total += i
-// }
-//return total
+
 //--------------------------------------------------------------------
 
-let total = 0;
-function sumToN(n) {
- // console.log(n)
-  if(n < 0){
-    return null
-  } else if (n === 0) {
-    console.log(total)
-    return total
-  } else {
-    total += n
-    sumToN(n - 1)
-    //console.log(n)
 
+function sumToN(n, sum = 0) {
+  if(n <= sum) {
+    return null
   }
+  if (n === 0) {
+    return sum
+  } else {
+    sum += n
+    return sumToN(n - 1, sum)
+  }
+  return sum
 }
 
-//console.log(sumToN(5)) // returns 15
-//console.log(sumToN(1))  // returns 1
-//console.log(sumToN(9))  // returns 45
-//console.log(sumToN(-8))  // returns null
+console.log(sumToN(5)) // returns 15
+// console.log(sumToN(1))  // returns 1
+// console.log(sumToN(9))  // returns 45
+console.log(sumToN(-8))  // returns null
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
