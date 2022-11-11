761e049f204bfe919be8e9857e6db299e6b0a211
Roberto Peregrina <111211515+RobertoPeregrinaJr96@users.noreply.github.com>
2022-11-10 19:58:07 -0800

    Update 15-subsets.js

--- practice-for-week-03-recursion-pt4-practice/problems/15-subsets.js
+++ practice-for-week-03-recursion-pt4-practice/problems/15-subsets.js
@@ -10,15 +10,41 @@ Hint: For subsets([1, 2, 3]), there are two kinds of subsets:
      subset that is the same, except it also does contain 3.
 ***********************************************************************/
 
+// const subsets = (arr) => {
+//   // Your code here
+//   if(arr[0] === undefined) return []
+//   if(arr[0]){
+
+//     return [arr[0], ...subsets(arr.slice(1))]
+//   }
+// }
+//-------------------------------------------------------
 const subsets = (arr) => {
-  // Your code here
-  if(arr[0] === undefined) return []
-  if(arr[0]){
-
-    return [arr[0], ...subsets(arr.slice(1))]
-  }
+  if (arr.length === 0) return [[]];
+  let last = arr[arr.length - 1];
+  let subs = subsets(arr.slice(0, arr.length - 1));
+  let newSubs = subs.map((sub) => {
+      return sub.concat(last);
+  });
+  return subs.concat(newSubs);
 }
+//--------------------------------------------------------------------------------
+// const subsets = (arr) => {
+//   if (arr.length === 0) return [[]];
 
+//   let current = arr.pop();
+
+//   let x = subsets(arr);
+
+//   let subArray = [];
+
+//   for (let i = 0; i < x.length; i++) {
+//     subArray.push([...x[i], current]);
+//   }
+
+//   return x.concat(subArray);
+// };
+//-------------------------------------------------------
 console.log(subsets([])) // [[]]
 console.log(subsets([1])) // [[], [1]]
 // console.log(subsets([1, 2])) // [[], [1], [2], [1, 2]]
